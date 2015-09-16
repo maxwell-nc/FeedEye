d22cc8569f7702df09b7eeff50d9f3f8f043871b
max <maxwell-nc@outlook.com>
2015-09-16 20:47:08 +0800

    添加了FeedPagerListViewItem类封装订阅页面的ListView中的Item类，更新了项目结构图

---
+++ src/pres/nc/maxwell/feedeye/view/FeedPagerListViewItem.java
@@ -0,0 +1,92 @@
+package pres.nc.maxwell.feedeye.view;
+
+import pres.nc.maxwell.feedeye.R;
+import android.app.Activity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+/**
+ * 封装FeedPager中的ListView的每一个Item对象View，提供子View的对象
+ */
+public class FeedPagerListViewItem {
+	
+	private Activity mActivity;
+	
+	private View mListViewItem;		//Item的View对象
+	
+	private ImageView mItemPic;		//图片
+	private TextView mItemTitle;	//订阅标题
+	private TextView mItemPreview;	//订阅预览
+	private TextView mItemTime;		//时间
+	private ImageView mItemCount;	//未读数
+	
+	public ImageView getmItemPic() {
+		return mItemPic;
+	}
+
+
+	public TextView getmItemTitle() {
+		return mItemTitle;
+	}
+
+
+	public TextView getmItemPreview() {
+		return mItemPreview;
+	}
+
+
+	public TextView getmItemTime() {
+		return mItemTime;
+	}
+
+
+	public ImageView getmItemCount() {
+		return mItemCount;
+	}
+
+
+	
+	
+	/**
+	 * 返回ListView的Item对象
+	 * @return ListView的Item对象
+	 */
+	public View getItemView(){
+		return mListViewItem;
+	}
+	
+	
+	/**
+	 * 构造函数
+	 * @param mActivity
+	 */
+	public FeedPagerListViewItem(Activity mActivity) {
+		super();
+		this.mActivity = mActivity;
+		
+		initListViewItem();
+	}
+
+
+	/**
+	 * 初始化ListView的Item的View对象
+	 */
+	private void initListViewItem() {
+		mListViewItem = View.inflate(mActivity, R.layout.view_lv_item_feed,
+				null);
+
+		mItemPic = (ImageView) mListViewItem
+				.findViewById(R.id.iv_item_feed_pic);
+		mItemTitle = (TextView) mListViewItem
+				.findViewById(R.id.tv_item_feed_title);
+		mItemPreview = (TextView) mListViewItem
+				.findViewById(R.id.tv_item_feed_preview);
+		mItemTime = (TextView) mListViewItem
+				.findViewById(R.id.tv_item_feed_time);
+		mItemCount = (ImageView) mListViewItem
+				.findViewById(R.id.iv_item_feed_count);
+	}
+
+}
--- src/pres/nc/maxwell/feedeye/view/pager/child/FeedPager.java
+++ src/pres/nc/maxwell/feedeye/view/pager/child/FeedPager.java
@@ -1,13 +1,14 @@
 package pres.nc.maxwell.feedeye.view.pager.child;
 
+import java.util.ArrayList;
+
 import pres.nc.maxwell.feedeye.R;
+import pres.nc.maxwell.feedeye.view.FeedPagerListViewItem;
 import pres.nc.maxwell.feedeye.view.pager.BasePager;
 import android.app.Activity;
-import android.database.DataSetObserver;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
 /**
@@ -17,7 +18,7 @@ public class FeedPager extends BasePager {
 
 	private View mViewContent; // 填充到父布局中的FrameLayout中的View对象
 	private ListView mListView;// 订阅列表
-
+	private ArrayList<FeedPagerListViewItem> mItemList;// ListView中的Item集合
 
 	public ListView getListView() {
 		return mListView;
@@ -34,37 +35,54 @@ public class FeedPager extends BasePager {
 		mTitle.setText("订阅列表");
 		mViewContent = setContainerContent(R.layout.pager_main_feed);
 		mListView = (ListView) mViewContent.findViewById(R.id.lv_feed_list);
+
+		mItemList = new ArrayList<FeedPagerListViewItem>();
+
 	}
-	
+
 	@Override
 	protected void initData() {
 		super.initData();
-		
-		//TODO:暂时测试填充数据
+
+		// TODO:暂时填充测试数据
+		for (int i = 0; i < 15; i++) {
+			FeedPagerListViewItem item = new FeedPagerListViewItem(mActivity);
+			mItemList.add(item);
+		}
+
 		mListView.setAdapter(new BaseAdapter() {
-			
+
 			@Override
 			public int getCount() {
 				return 15;
 			}
-			
+
 			@Override
 			public View getView(int position, View convertView, ViewGroup parent) {
-				View view = View.inflate(mActivity, R.layout.view_lv_item_feed, null);
-				return view;
+				
+				// TODO:暂时填充测试数据
+				FeedPagerListViewItem item = mItemList.get(position);
+				item.getmItemTitle().setText("测试对象是否正确");
+				if (position == 5) {
+
+					item.getmItemPic().setImageDrawable(
+							mActivity.getResources().getDrawable(
+									R.drawable.ic_launcher));
+
+				}
+				return item.getItemView();
 			}
-			
+
 			@Override
 			public long getItemId(int position) {
 				return 0;
 			}
-			
+
 			@Override
 			public Object getItem(int position) {
 				return null;
 			}
-			
-			
+
 		});
 	}
 
