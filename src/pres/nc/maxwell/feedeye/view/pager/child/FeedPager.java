c4960ce3eeab823ff7848fe467bfb0c42748c129
max <maxwell-nc@outlook.com>
2015-09-16 23:04:54 +0800

    添加了订阅列表的ListView下拉刷新布局，修改项目结构图 TODO：添加下拉刷新动画和触摸逻辑

--- src/pres/nc/maxwell/feedeye/view/pager/child/FeedPager.java
+++ src/pres/nc/maxwell/feedeye/view/pager/child/FeedPager.java
@@ -3,12 +3,12 @@ package pres.nc.maxwell.feedeye.view.pager.child;
 import java.util.ArrayList;
 
 import pres.nc.maxwell.feedeye.R;
-import pres.nc.maxwell.feedeye.utils.LogUtils;
 import pres.nc.maxwell.feedeye.view.FeedPagerListViewItem;
 import pres.nc.maxwell.feedeye.view.pager.BasePager;
 import android.app.Activity;
-import android.content.ContentValues;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
@@ -22,8 +22,12 @@ import android.widget.TextView;
 public class FeedPager extends BasePager {
 
 	private View mViewContent; // 填充到父布局中的FrameLayout中的View对象
+	
 	private ListView mListView;// 订阅列表
 	private ArrayList<FeedPagerListViewItem> mItemList;// ListView中的Item集合
+	
+	private View headerView;
+	private int headerViewHeight;
 
 	public ListView getListView() {
 		return mListView;
@@ -55,88 +59,122 @@ public class FeedPager extends BasePager {
 			mItemList.add(item);
 		}
 
-		mListView.setAdapter(new BaseAdapter() {
+		headerView = View.inflate(mActivity,
+				R.layout.view_header_listview_refresh, null);
+		mListView.addHeaderView(headerView);
+
+		// 默认隐藏HeaderView
+		headerView.measure(0, 0);// headerView根节点不能为RelativeLayout，否则空指针异常
+		headerViewHeight = headerView.getMeasuredHeight();
+		headerView.setPadding(0, -headerViewHeight, 0, 0);
+
+		//设置ListView适配器
+		mListView.setAdapter(new FeedPagerListViewAdapter());
+
+		//设置ListView触摸事件
+		mListView.setOnTouchListener(new OnTouchListener() {
 
 			@Override
-			public int getCount() {
-				return 150;
-			}
-
-			@Override
-			public View getView(int position, View convertView, ViewGroup parent) {
+			public boolean onTouch(View v, MotionEvent event) {
+				//点击时Y坐标
+				float y = event.getY();
 				
-				RelativeLayout view;
-				ViewHolder holder;
-				
-				
-				//复用ConvertView
-				if(convertView!=null && convertView instanceof RelativeLayout){
-					//复用View并取出holder
-					view = (RelativeLayout) convertView;
-					holder = (ViewHolder) view.getTag();
-					
-					holder.mItemPic.setImageDrawable(
-							mActivity.getResources().getDrawable(
-									R.drawable.btn_navi_favor_selected));
-					
-					//检查是否复用ConvertView，平时不需要打印，费时
-					//LogUtils.v("FeedPager", "复用View");
-					
-				}else{
-					//不可复用
-					
-					// TODO:暂时填充测试数据
-					FeedPagerListViewItem item = mItemList.get(position);
-					item.getItemTitle().setText("测试对象是否正确");
-					if (position == 5) {
+				switch (event.getAction()) {
+				case MotionEvent.ACTION_DOWN:
+					break;
 
-						item.getItemPic().setImageDrawable(
-								mActivity.getResources().getDrawable(
-										R.drawable.ic_launcher));
-
-					}
-					view = (RelativeLayout) item.getItemView();
+				case MotionEvent.ACTION_MOVE:
+					//TODO:完成下拉
 					
-					//利用ViewHolder记录子孩子View对象
-					holder = new ViewHolder();
+					break;
+				case MotionEvent.ACTION_UP:
 					
-					holder.mItemPic = item.getItemPic();
-					holder.mItemTitle = item.getItemTitle();
-					holder.mItemPreview = item.getItemPreview();
-					holder.mItemTime = item.getItemTime();
-					holder.mItemCount = item.getItemCount();
-					
-					view.setTag(holder);
+					break;
 				}
-				
-				
-				return view;
-			}
 
-			@Override
-			public long getItemId(int position) {
-				return 0;
+				return false;
 			}
-
-			@Override
-			public Object getItem(int position) {
-				return null;
-			}
-
 		});
 	}
 
-	
 	/**
-	 * 利用ViewHolder优化ListView，减少findViewById的次数 
+	 * 利用ViewHolder优化ListView，减少findViewById的次数
 	 */
-	static class ViewHolder{
-		public ImageView mItemPic;		//图片
-		public TextView mItemTitle;		//订阅标题
-		public TextView mItemPreview;	//订阅预览
-		public TextView mItemTime;		//时间
-		public ImageView mItemCount;	//未读数
+	static class ViewHolder {
+		public ImageView mItemPic; // 图片
+		public TextView mItemTitle; // 订阅标题
+		public TextView mItemPreview; // 订阅预览
+		public TextView mItemTime; // 时间
+		public ImageView mItemCount; // 未读数
 	}
-	
-}
 
+	/**
+	 * 订阅列表的适配器
+	 */
+	class FeedPagerListViewAdapter extends BaseAdapter {
+
+		@Override
+		public int getCount() {
+			return 150;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+
+			RelativeLayout view;
+			ViewHolder holder;
+
+			// 复用ConvertView
+			if (convertView != null && convertView instanceof RelativeLayout) {
+				// 复用View并取出holder
+				view = (RelativeLayout) convertView;
+				holder = (ViewHolder) view.getTag();
+
+				holder.mItemPic.setImageDrawable(mActivity.getResources()
+						.getDrawable(R.drawable.btn_navi_favor_selected));
+
+				// 检查是否复用ConvertView，平时不需要打印，费时
+				// LogUtils.v("FeedPager", "复用View");
+
+			} else {
+				// 不可复用
+
+				// TODO:暂时填充测试数据
+				FeedPagerListViewItem item = mItemList.get(position);
+				item.getItemTitle().setText("测试对象是否正确");
+				if (position == 5) {
+
+					item.getItemPic().setImageDrawable(
+							mActivity.getResources().getDrawable(
+									R.drawable.ic_launcher));
+
+				}
+				view = (RelativeLayout) item.getItemView();
+
+				// 利用ViewHolder记录子孩子View对象
+				holder = new ViewHolder();
+
+				holder.mItemPic = item.getItemPic();
+				holder.mItemTitle = item.getItemTitle();
+				holder.mItemPreview = item.getItemPreview();
+				holder.mItemTime = item.getItemTime();
+				holder.mItemCount = item.getItemCount();
+
+				view.setTag(holder);
+			}
+
+			return view;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return 0;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return null;
+		}
+
+	}
+}
